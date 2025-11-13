1b5611681145c6ffde29fdb4e8c8c1503db7627d
Viraj Samarasinghe <samarasinghe_nacv_e23@engug.ruh.ac.lk>
2025-11-13 22:40:28 +0530

    Remove Vitest configuration file and add .DS_Store to the repository

--- src/libs/I18nNavigation.ts
+++
@@ -1,4 +0,0 @@
-import { createNavigation } from 'next-intl/navigation';
-import { routing } from './I18nRouting';
-
-export const { usePathname } = createNavigation(routing);
--- src/libs/I18nRouting.ts
+++
@@ -1,8 +0,0 @@
-import { defineRouting } from 'next-intl/routing';
-import { AppConfig } from '@/utils/AppConfig';
-
-export const routing = defineRouting({
-  locales: AppConfig.locales,
-  localePrefix: AppConfig.localePrefix,
-  defaultLocale: AppConfig.defaultLocale,
-});
